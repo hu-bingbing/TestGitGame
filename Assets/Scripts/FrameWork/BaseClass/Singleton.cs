438b268564b66e8d521e7859daa48a02de7c5a55
bing.hu <bing.hu@cytxcn.com>
2017-11-30 17:43:36 +0800

    xbar

---
+++ Assets/Scripts/FrameWork/BaseClass/Singleton.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Reflection;
+public abstract class Singleton<T> where T : class
+{
+    private static T m_intance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (m_intance == null)
+            {
+                m_intance = null;
+                Type type = typeof(T);
+                // public item should cut it out
+                BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public;
+                ConstructorInfo[] constructorInfoArray = type.GetConstructors(bindingAttr);
+
+                foreach (ConstructorInfo constructorInfo in constructorInfoArray)
+                {
+                    ParameterInfo[] parameterInfoArray = constructorInfo.GetParameters();
+                    if (parameterInfoArray.Length == 0)
+                    {
+                        m_intance = (T)constructorInfo.Invoke(null);
+                        break;
+                    }
+                }
+
+                if (null == m_intance)
+                {
+                    throw new NotSupportedException("No NonPublic constructor without 0 parameter");
+                }
+            }
+
+            return m_intance;
+        }
+    }
+
+    protected Singleton()
+    {
+    }
+}
+
+//public class Singleton<T>where T :new()
+//{
+//    public static T Instance
+//    {
+//        get
+//        {
+//            return SingletonCreator.instance;
+//        }
+//    }
+
+//	protected Singleton()
+//    {
+//        if(Instance != null)
+//        {
+//            throw (new Exception("You have tried to create a new singleton class where you should have instanced it.Replace your \"new class()\" with \"class.Instance\""));
+//        }
+//    }
+
+//    class SingletonCreator
+//    {
+//        static SingletonCreator()
+//        {
+
+//        }
+//        internal static readonly T instance = new T();
+//    }
+//}
+//using System;
---
+++ Assets/Scripts/FrameWork/BaseClass/SingletonMono.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingletonMono<T> : MonoBehaviour where T : MonoBehaviour
+{
+    protected static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+                if(_instance != null)
+                {
+                    return _instance;
+                }
+                GameObject go = new GameObject(typeof(T).ToString());
+                _instance = go.AddComponent<T>();
+            }
+            return _instance;
+        }
+    }
+
+    //public virtual void Awake()
+    //{
+    //    if (_instance == null)
+    //    {
+    //        //If I am the first instance, make me the Singleton
+    //        _instance = this.GetComponent<T>();
+    //        DontDestroyOnLoad(this);
+    //    }
+    //    else
+    //    {
+    //        //If a Singleton already exists and you find
+    //        //another reference in scene, destroy it!
+    //        if (this != _instance)
+    //            Destroy(this.gameObject);
+    //    }
+    //}
+}
